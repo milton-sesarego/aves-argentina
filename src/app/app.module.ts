dbce83a5325e33bced4f2c4c9489d09115177d69
Milton Sesarego <milton.sesarego@gmail.com>
2019-10-04 18:07:53 -0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TabComponent } from './tab.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { FirebaseService } from './firebase.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule, HttpClientModule, MaterialModule, BrowserAnimationsModule,
+  MatTabsModule, FontAwesomeModule ,
+  AngularFireModule.initializeApp(environment.firebase),
+  AngularFirestoreModule],
+  declarations: [ AppComponent, HelloComponent, TabComponent],
+  bootstrap:    [ AppComponent ],
+  providers: [ AngularFirestore, FirebaseService ]
 })
 export class AppModule { }
+
